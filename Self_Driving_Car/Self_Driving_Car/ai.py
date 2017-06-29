60fa25c2903a1cd40c3da747ccea92381f1e56cd
Thanee Ananitthi <sawatdeekrab@gmail.com>
2017-06-29 20:54:17 +0700

    update code

--- Self_Driving_Car/Self_Driving_Car/ai.py
+++ Self_Driving_Car/Self_Driving_Car/ai.py
@@ -12,5 +12,21 @@ import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
+# Create Architecture of Neural Network
 
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+    
+    def forward(self, state):
+        x = F.relu(self.fc1(state)
+        q_values = self.fc2(x)
+        return q_values
+    
+# Implementing Experience Replay
 
